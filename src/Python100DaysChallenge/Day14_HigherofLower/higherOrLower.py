fffd06c5b43cc255b30cd8d706ee9e4f18fb75b0
Girijasankar <yagneswarasankar@gmail.com>
2021-02-26 23:09:33 +0530

    HigherOrLower game cleaningUP But still can be improved

--- src/Python100DaysChallenge/Day14_HigherofLower/higherOrLower.py
+++ src/Python100DaysChallenge/Day14_HigherofLower/higherOrLower.py
@@ -2,20 +2,14 @@ import random
 from src.Python100DaysChallenge.Day14_HigherofLower.art import logo,vs
 from src.Python100DaysChallenge.Day14_HigherofLower.gameData import data
 
-def getRandomInt():
-    return random.randint(0,len(data) -1)
-lengthOfDict = len(data)
-random_number = getRandomInt()
+
 should_continue = True
 numberOfPoints = 0
-compareElement = getRandomInt()
-compareElementData = data[compareElement]
-#while should_continue:
-comparableElement = getRandomInt()
-comparableElementData = data[comparableElement]
-print(f'{compareElementData["name"]},{compareElementData["description"]} from {compareElementData["country"]}')
+compareElementData = random.choice(data)
+comparableElementData = random.choice(data)
+print(f'Compare A: {compareElementData["name"]},{compareElementData["description"]} from {compareElementData["country"]}')
 print(vs)
-print(f'{comparableElementData["name"]},{comparableElementData["description"]} from {comparableElementData["country"]}')
+print(f'Against B: {comparableElementData["name"]},{comparableElementData["description"]} from {comparableElementData["country"]}')
 def findMax(first,second):
     if first["follower_count"] > second["follower_count"]:
         return "A"
@@ -26,12 +20,12 @@ while should_continue:
     if guess == findMax(compareElementData,comparableElementData) :
         print("findMax" + findMax(compareElementData,comparableElementData))
         numberOfPoints +=1
-        compareElementData = data[comparableElement]
-        print(f'{compareElementData["name"]},{compareElementData["description"]} from {compareElementData["country"]}')
-        comparableElement = getRandomInt()
-        comparableElementData = data[comparableElement]
+        print(f"you're right! Current Score : {numberOfPoints}")
+        compareElementData = comparableElementData
+        print(f'Compare A: {compareElementData["name"]},{compareElementData["description"]} from {compareElementData["country"]}')
+        comparableElementData = random.choice(data)
         print(vs)
-        print(f'{comparableElementData["name"]},{comparableElementData["description"]} from {comparableElementData["country"]}')
+        print(f'Agaginst B: {comparableElementData["name"]},{comparableElementData["description"]} from {comparableElementData["country"]}')
     else:
         print(f"Sorry, that's wrong. Final score: {numberOfPoints}")
         should_continue = False
